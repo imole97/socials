8ebb592f95ee6bf437e2e6b56648e3c4a1f0fac3
Ropo Olatujoye <iamfiropo@gmail.com>
2022-02-23 11:07:57 +0100

    add getall add getone add update endpoint add delete endpoint

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -11,6 +11,52 @@ class PostController {
 
         res.status(201).json(post);
     }
+
+    static getAll = async (req, res) => {
+        const posts = await Post.find();
+
+        res.status(200).json(posts);
+    }
+
+    static getOne = async (req, res) => {
+        const { id } = req.params;
+
+        const post = await Post.findOne({ _id: id })
+
+        res.status(200).json(post)
+    }
+
+    static update = async (req, res) => {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(404).json('Post not found')
+        }
+
+        const post = await Post.findOne({ _id: id });
+        const updatedPost = Object.assign(post, req.body);
+
+        updatedPost.save();
+
+        res.status(200).json(updatedPost);
+
+    }  
+
+    static delete = async (req, res) => {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json('Invalid request')
+        }
+
+        const post = await Post.findByIdAndDelete({ _id: id });
+
+        if (!post) {
+            return res.status(404).json('Post not found')
+        }
+
+        res.status(200).json('success')
+    }
 }
 
 module.exports = PostController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ const PostController = require('../controllers/post.controller');
 
 module.exports = router => {
     router.post('/posts', PostController.create)
+    router.get('/posts', PostController.getAll)
+    router.get('/posts/:id', PostController.getOne)
+    router.patch('/posts/:id', PostController.update)
+    router.delete('/posts/:id', PostController.delete)
 
     return router;
 }
